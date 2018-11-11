7db7b6c458525096431a6a771f1f8943f7fbeda2
Mane <43752233+maannnne@users.noreply.github.com>
2018-11-11 19:50:04 +0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -25,27 +25,30 @@ window.onload = () => {
    let bigCircle = document.createElement("div");
    container.appendChild(bigCircle);
    bigCircle.setAttribute('style','width: 400px; height: 400px; border: 2px solid #4d564d; border-radius: 50%; position: fixed;');
+   let big = bigCircle.getBoundingClientRect();
+   let bigCircleRadius = bigCircle.style.width.split('px')[0]/2; //2 ժամ ա քեզ գտնելու ձևն եմ ման գալիս, անամոթ -_-
+   let big = bigCircle.getBoundingClientRect();
    
 //small ball
    let smallBall = document.createElement("div");
    container.appendChild(smallBall);
    smallBall.setAttribute('style', 'height: 34px; width: 34px; border-radius: 50%;  position: fixed; background-color: #3366b7;');
+   let smallBallRadius = smallBall.style.width.split('px')[0]/2;
+   smallBall.style.left = big.right - smallBallRadius + 'px';
+   smallBall.style.top = big.bottom/2 + 'px';
+   //console.log(big.left, big.top, big.right, big.bottom);
 
+   
    let angle = 0;
    let cx;  //smallBall center x
    let cy;  //smallBall center y
    let stop_start;  //for set/clear time out
-   let bigCircleRadius = bigCircle.style.width.split('px')[0]/2; //2 ժամ ա քեզ գտնելու ձևն եմ ման գալիս, անամոթ -_-
-
 
 //main spinIt() function   
     
    startButton.onclick = spinIt = () => {
        document.getElementById('startButtonId').disabled = true;
-       let big = bigCircle.getBoundingClientRect();
-       //console.log(big.top, big.right, big.bottom, big.left);
 
-       let smallBallRadius = smallBall.style.width.split('px')[0]/2;
        let centerX = bigCircleRadius + big.left;
        let centerY = bigCircleRadius + big.top; 
        cx = centerX - smallBallRadius + bigCircleRadius * Math.cos(angle * Math.PI/ 180); 
